1f0c7ccef9f43ef933f955d54d441e92a5006fae
Clément <clement.grosjean@live.fr>
2016-04-14 00:01:01 +0200

    v 0.2

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import math
 import random
+import time
+
 
 # i = 5
 # secret = 42
@@ -11,13 +13,13 @@ import random
 def reponse(message, mentir):
     if message == 42:
         print("Bravo tu as réussi")
-    elif message == 1 and mentir == True:## 1 == trop grand
+    elif message == 1 and mentir == True:  ## 1 == trop grand
         print("nombre petit, recommence cela ne fait que la {0} fois que tu recommence".format(i))
-    elif message == 1 and mentir == False:## 1 == trop grand
+    elif message == 1 and mentir == False:  ## 1 == trop grand
         print("nombre  grand, recommence cela ne fait que la {0} fois que tu recommence".format(i))
-    elif message == 2 and mentir == True:## 2 == trop petit
+    elif message == 2 and mentir == True:  ## 2 == trop petit
         print("nombre  grand, recommence cela ne fait que la {0} fois que tu recommence".format(i))
-    else:##2 ==trop petit
+    else:  ##2 == trop petit
         print("nombre petit, recommence cela ne fait que la {0} fois que tu recommence".format(i))
 
 
@@ -26,19 +28,37 @@ def message(secret, nombre):
         return 42
     elif secret < nombre:
         return 1
-    else: return 2
+    else:
+        return 2
+
 
 def aleat(level):
-    return random.randint(0,level)
+    return random.randint(0, level)
+
 
 def liste_mensonge(taux_mensonge):
     B, k = [], 0
     A = [False for i in range(100)]
     while k <= taux_mensonge:
-        aleat_mensonge = random.randint(0,99)
+        aleat_mensonge = random.randint(0, 99)
         if B.count(aleat_mensonge) == 0:
             B += [aleat_mensonge]
-            A[aléat_mensonge] = True
+            A[aleat_mensonge] = True
             k += 1
         else:
             pass
+    return A
+
+
+def exponentiation_rapide(x, n):
+    if n == 1:
+        return x
+    elif n % 2 == 0:
+        return exponentiation_rapide(x * x, n / 2)
+    return x * exponentiation_rapide(x * x, (n - 1) / 2)
+
+
+def mentir(A, t):
+    a = random.randint(0, 99)
+    t1 = (time.perf_counter() - t) * exponentiation_rapide(10, 16) + a
+    p = t1 % len(A)
