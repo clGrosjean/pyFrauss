48b175c26e2a2b18239c6e5329ba1e8c95fd709d
Clément <clement.grosjean@live.fr>
2016-04-13 22:27:23 +0200

    v 0.1

---
+++ main.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+
+import math
+import random
+
+# i = 5
+# secret = 42
+# nombre = 40
+# mentir = True
+# message = 42
+def reponse(message, mentir):
+    if message == 42:
+        print("Bravo tu as réussi")
+    elif message == 1 and mentir == True:## 1 == trop grand
+        print("nombre petit, recommence cela ne fait que la {0} fois que tu recommence".format(i))
+    elif message == 1 and mentir == False:## 1 == trop grand
+        print("nombre  grand, recommence cela ne fait que la {0} fois que tu recommence".format(i))
+    elif message == 2 and mentir == True:## 2 == trop petit
+        print("nombre  grand, recommence cela ne fait que la {0} fois que tu recommence".format(i))
+    else:##2 ==trop petit
+        print("nombre petit, recommence cela ne fait que la {0} fois que tu recommence".format(i))
+
+
+def message(secret, nombre):
+    if secret == nombre:
+        return 42
+    elif secret < nombre:
+        return 1
+    else: return 2
+
+def aléat(level):
+    return random.randint(0,level)
+
+def liste_mensonge(taux_mensonge):
+    B, k = [], 0
+    A = [False for i in range(100)]
+    while k <= taux_mensonge:
+        aléat_mensonge = random.randint(0,100)
+        if B.count(aléat_mensonge) == 0:
+            B += [aléat_mensonge]
+            A[aléat_mensonge] = True
+            k += 1
+        else:
+            pass
